9fe6df1e045a33adee58dd7f06169bb6f355a634
Bruno Vieira <Bruno.Vieira.Rosa@ibm.com>
2019-02-01 17:32:18 -0200

    Adding fontawesome

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,18 @@ export default {
 </script>
 
 <style>
+@import '../node_modules/font-awesome/css/font-awesome.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+i {
+  font-family: fontawesome;
+  font-style: normal;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,19 @@
       <h1 class="navbar-item title is-4">HYDROSYS</h1>
     </div>
     <div class="navbar-end">
-      <h1 class="navbar-item title is-4">Facebook</h1>
+      <a class="navbar-item" href="https://www.facebook.com/brunosvieira" target="_blank">
+        <i class="fab fa-facebook-square fa-2x"></i>
+      </a>
+      <a class="navbar-item" href="https://github.com/brvieira" target="_blank">
+        <i class="fab fa-github fa-2x"></i>
+      </a>
+      <a
+        class="navbar-item"
+        href="https://www.linkedin.com/in/bruno-vieira-33481910b/"
+        target="_blank"
+      >
+        <i class="fab fa-linkedin fa-2x"></i>
+      </a>
     </div>
   </nav>
 </template>
@@ -17,5 +29,9 @@ export default {
 .navbar {
   margin-bottom: 3rem;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+}
+
+.title {
+  font-family: sans-serif
 }
 </style>
